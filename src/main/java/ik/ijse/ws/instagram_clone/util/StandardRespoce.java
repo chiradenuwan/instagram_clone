4a30ec109b6216130a207a73a5c8e82c144dcf69
chira <chiranjayadenuwan@gmail.com>
2020-12-05 14:15:44 +0530

    create a responce class

---
+++ src/main/java/ik/ijse/ws/instagram_clone/util/StandardRespoce.java
@@ -0,0 +1,49 @@
+package ik.ijse.ws.instagram_clone.util;
+
+public class StandardRespoce {
+    private int code;
+    private String message;
+    private Object object;
+
+    public StandardRespoce() {
+    }
+
+    public StandardRespoce(int code, String message, Object object) {
+        this.code = code;
+        this.message = message;
+        this.object = object;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    @Override
+    public String toString() {
+        return "StandardRespoce{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                ", object=" + object +
+                '}';
+    }
+}
