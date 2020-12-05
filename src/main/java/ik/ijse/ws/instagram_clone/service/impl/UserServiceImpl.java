874c4b28145c3a7be3885fbc684d00da2f3407c7
chira <chiranjayadenuwan@gmail.com>
2020-12-05 15:38:42 +0530

    create user service for register user

--- src/main/java/ik/ijse/ws/instagram_clone/service/impl/UserServiceImpl.java
+++ src/main/java/ik/ijse/ws/instagram_clone/service/impl/UserServiceImpl.java
@@ -1,4 +1,46 @@
 package ik.ijse.ws.instagram_clone.service.impl;
 
-public class UserServiceImpl {
+import ik.ijse.ws.instagram_clone.dto.UserDto;
+import ik.ijse.ws.instagram_clone.entity.User;
+import ik.ijse.ws.instagram_clone.repository.UserRepository;
+import ik.ijse.ws.instagram_clone.service.UserService;
+import ik.ijse.ws.instagram_clone.util.StandardResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public StandardResponse registerUser(UserDto userDto) throws Exception {
+        if (userRepository.existsByUsername(userDto.getusername())) {
+            return new StandardResponse(500, "User Already Exists", null);
+        }
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setUsername(userDto.getusername());
+        user.setPassword(userDto.getPassword());
+        user.setPro_pic(userDto.getPro_pic());
+        User save = userRepository.save(user);
+        System.out.println(save);
+
+        return null;
+    }
+
+//    @Override
+//    public boolean loginUser(UserDto userDto) throws Exception {
+//        User user = new User();
+//        user.setUsername(userDto.getusername());
+//        user.setPassword(userDto.getPassword());
+//        User user1 = userRepository.loginUser(user);
+//        System.out.println(user1);
+//        return false;
+//    }
 }
