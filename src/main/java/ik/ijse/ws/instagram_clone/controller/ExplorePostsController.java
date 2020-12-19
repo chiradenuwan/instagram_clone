19a52bdabbcb8ca3568bdd8c743cd321a665ddc2
chira <chiranjayadenuwan@gmail.com>
2020-12-19 13:36:53 +0530

    Create a controller to explore posts

---
+++ src/main/java/ik/ijse/ws/instagram_clone/controller/ExplorePostsController.java
@@ -0,0 +1,11 @@
+package ik.ijse.ws.instagram_clone.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/explorepost")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ExplorePostsController {
+}
