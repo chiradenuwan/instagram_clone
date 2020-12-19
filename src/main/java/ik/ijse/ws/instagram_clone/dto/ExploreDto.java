51240f7c98eac0e8d0bae565c71964e5d39d5a06
chira <chiranjayadenuwan@gmail.com>
2020-12-19 14:44:46 +0530

    update ExploreDto

--- src/main/java/ik/ijse/ws/instagram_clone/dto/ExploreDto.java
+++ src/main/java/ik/ijse/ws/instagram_clone/dto/ExploreDto.java
@@ -1,34 +1,36 @@
 package ik.ijse.ws.instagram_clone.dto;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 public class ExploreDto {
-    private String description;
-    private String image_url;
+    private List<String> description;
+    private List<String> image_url;
     private Timestamp date_time;
+
 
     public ExploreDto() {
     }
 
-    public ExploreDto(String description, String image_url, Timestamp date_time) {
+    public ExploreDto(List<String> description, List<String> image_url, Timestamp date_time) {
         this.description = description;
         this.image_url = image_url;
         this.date_time = date_time;
     }
 
-    public String getDescription() {
+    public List<String> getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
+    public void setDescription(List<String> description) {
         this.description = description;
     }
 
-    public String getImage_url() {
+    public List<String> getImage_url() {
         return image_url;
     }
 
-    public void setImage_url(String image_url) {
+    public void setImage_url(List<String> image_url) {
         this.image_url = image_url;
     }
 
