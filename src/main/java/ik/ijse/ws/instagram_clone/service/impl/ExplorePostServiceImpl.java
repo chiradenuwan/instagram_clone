d5bfce37335b79154108baed5778f2443fa4b253
chira <chiranjayadenuwan@gmail.com>
2020-12-19 13:57:02 +0530

    Create explore Services

---
+++ src/main/java/ik/ijse/ws/instagram_clone/service/impl/ExplorePostServiceImpl.java
@@ -0,0 +1,11 @@
+package ik.ijse.ws.instagram_clone.service.impl;
+
+import ik.ijse.ws.instagram_clone.service.ExplorePostService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ExplorePostServiceImpl implements ExplorePostService {
+}
