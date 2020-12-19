a47a8e64d6aff00b56205c353b02bd6e2c3be9a9
chira <chiranjayadenuwan@gmail.com>
2020-12-19 14:45:39 +0530

    Make get all function in ExplorePostService

--- src/main/java/ik/ijse/ws/instagram_clone/service/ExplorePostService.java
+++ src/main/java/ik/ijse/ws/instagram_clone/service/ExplorePostService.java
@@ -1,4 +1,9 @@
 package ik.ijse.ws.instagram_clone.service;
 
+import ik.ijse.ws.instagram_clone.entity.ExplorePost;
+
+import java.util.List;
+
 public interface ExplorePostService {
+    List<ExplorePost> loadAllPosts(int offset, int limit,int userId);
 }
--- src/main/java/ik/ijse/ws/instagram_clone/service/impl/ExplorePostServiceImpl.java
+++ src/main/java/ik/ijse/ws/instagram_clone/service/impl/ExplorePostServiceImpl.java
@@ -1,11 +1,22 @@
 package ik.ijse.ws.instagram_clone.service.impl;
 
+import ik.ijse.ws.instagram_clone.entity.ExplorePost;
+import ik.ijse.ws.instagram_clone.repository.ExplorePostRepository;
 import ik.ijse.ws.instagram_clone.service.ExplorePostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
 public class ExplorePostServiceImpl implements ExplorePostService {
+    @Autowired
+    private ExplorePostRepository explorePostRepository;
+    @Override
+    public List<ExplorePost> loadAllPosts(int offset, int limit,int userId) {
+        explorePostRepository.getAllPosts(offset,limit,userId);
+        return null;
+    }
 }
