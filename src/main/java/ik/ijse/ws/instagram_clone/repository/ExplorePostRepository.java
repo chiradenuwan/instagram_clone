84586ba97dde02e425c8357c128cf49a70c3d78c
chira <chiranjayadenuwan@gmail.com>
2020-12-19 14:45:21 +0530

    Make get all function in ExplorePostRepository

--- src/main/java/ik/ijse/ws/instagram_clone/repository/ExplorePostRepository.java
+++ src/main/java/ik/ijse/ws/instagram_clone/repository/ExplorePostRepository.java
@@ -1,7 +1,21 @@
 package ik.ijse.ws.instagram_clone.repository;
 
 import ik.ijse.ws.instagram_clone.entity.ExplorePost;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ExplorePostRepository extends JpaRepository<ExplorePost, Integer> {
+
+    @Autowired
+    ExplorePost explorepost = null;
+
+
+
+    @Query(value = "  SELECT * FROM exploreposts ep WHERE NOT u_id =:u_id ", nativeQuery = true)
+    List<ExplorePost> getAllPosts(int offset, int limit,@Param("u_id")int userId);
+
 }
