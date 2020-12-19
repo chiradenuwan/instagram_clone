73ad5a6669f4d3e7c7d6974e544ff22ce878ffce
chira <chiranjayadenuwan@gmail.com>
2020-12-19 13:51:09 +0530

    Create explorepost repository

---
+++ src/main/java/ik/ijse/ws/instagram_clone/repository/ExplorePostRepository.java
@@ -0,0 +1,7 @@
+package ik.ijse.ws.instagram_clone.repository;
+
+import ik.ijse.ws.instagram_clone.entity.ExplorePost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExplorePostRepository extends JpaRepository<ExplorePost, Integer> {
+}
