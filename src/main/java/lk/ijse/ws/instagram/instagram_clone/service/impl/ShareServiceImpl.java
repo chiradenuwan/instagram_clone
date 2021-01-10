e00301faba6a5c4f47da6322396dedb1d1a0d9ab
chira <chiranjayadenuwan@gmail.com>
2021-01-10 12:01:39 +0530

    begin share component

---
+++ src/main/java/lk/ijse/ws/instagram/instagram_clone/service/impl/ShareServiceImpl.java
@@ -0,0 +1,13 @@
+package lk.ijse.ws.instagram.instagram_clone.service.impl;
+
+
+import lk.ijse.ws.instagram.instagram_clone.service.ShareService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class ShareServiceImpl implements ShareService {
+
+}
